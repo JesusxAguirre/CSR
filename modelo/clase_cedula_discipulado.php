f4a4dab5c5e62d9a80f4afb2e022c576a887ce36
JesusxAguirre <99jesus.aguirre@gmail.com>
2022-08-29 14:17:28 -0400

    funcion de registrar discipulado finalizada V1 testeando

--- modelo/clase_cedula_discipulado.php
+++ modelo/clase_cedula_discipulado.php
@@ -79,23 +79,6 @@ private $participantes;
             ":cedula" => $this->cedula_lider
         ));
 
-        //comienzo de funcion de pasar id foraneo con respecto a la cedula en este caso primero del lider
-        //primero vamos a buscar el id que queremos pasar como clave foranea
-
-        $sql = ("SELECT id FROM celula_discipulado 
-        WHERE cedula_lider= '$this->cedula_lider'
-        AND cedula_anfitrion = '$this->cedula_anfitrion'
-        AND cedula_asistente = '$this->cedula_asistente'");
-       
-       $stmt = $this->conexion()->prepare($sql);
-       
-        $stmt->execute(array());
-       
-        $id_discipulado  = $stmt->fetch(PDO::FETCH_ASSOC);
-       
-        $id_discipulado = $id_discipulado['id'];
-        $sql = ("UPDATE usuarios SET id_discipulado = :id WHERE cedula = :cedula");
-
         //comprobando que el anfitrion y el asistente sean la misma cedula
         if ($this->cedula_anfitrion == $this->cedula_asistente) {
             $sql = ("SELECT codigo FROM usuarios WHERE cedula = '$this->cedula_anfitrion'");
@@ -149,7 +132,39 @@ private $participantes;
                 ":cedula" => $this->cedula_asistente
             ));
         }
+
+
+
+        //---------Comienzo de funcion de pasar id foraneo con respecto a los participantes de la celula------------------------//
+        //primero vamos a buscar el id que queremos pasar como clave foranea
+
+        $sql = ("SELECT id FROM celula_discipulado 
+        WHERE cedula_lider= '$this->cedula_lider'
+        AND cedula_anfitrion = '$this->cedula_anfitrion'
+        AND cedula_asistente = '$this->cedula_asistente'");
+       
+       $stmt = $this->conexion()->prepare($sql);
+       
+        $stmt->execute(array());
+       
+        $id_discipulado  = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+        foreach($this->participantes as $participantes){
+        $sql = ("UPDATE usuarios SET id_discipulado = :id WHERE cedula = :cedula");
+
+        $stmt = $this->conexion()->prepare($sql);
+
+        $stmt->execute(array(
+            ":id" => $id_discipulado['id'],
+            ":cedula" => $participantes
+        ));
+        }//fin del foreach
+        //id foraneo agregado por cada participante
+
+
+
     }
+
     //-------- SET DATOS Para registar discipulado-------------------------------------//
   public function setDiscipulado($cedula_lider, $cedula_anfitrion, $cedula_asistente, $dia, $hora, $direccion,$participantes)
   {
@@ -166,6 +181,3 @@ private $participantes;
 
 
 }
-
-
-?>
