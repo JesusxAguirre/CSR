2a775a64919db8ad5382e2e420a5d3690226c668
JesusxAguirre <99jesus.aguirre@gmail.com>
2022-09-08 10:54:30 -0400

    creados los archivos de reportar consolidacion

---
+++ controlador/reporte-celula-consolidacion.php
@@ -0,0 +1,27 @@
+<?php
+
+//destruye la sesion si se tenia una abierta
+session_start();
+require_once('modelo/clase_celula_consolidacion.php');
+if ($_SESSION['verdadero'] > 0) {
+    if (is_file('vista/' . $pagina . '.php')) {
+      $objeto = new Consolidacion();
+
+      $matriz_codigo = $objeto->listar_celula_consolidacion();
+    
+        require_once 'vista/' . $pagina . '.php';
+    }
+} else {
+    echo "<script>
+           alert('Inicia sesion ');
+           window.location= 'index.php'
+</script>";
+}
+if (isset($_POST['cerrar'])) {
+    session_destroy();
+    echo "<script>
+    alert('Sesion Cerrada');
+    window.location= 'index.php'
+</script>";
+}
+?>
