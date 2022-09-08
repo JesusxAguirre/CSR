38036b406b578a2d3baa06436b6388baa7f8b58f
JesusxAguirre <99jesus.aguirre@gmail.com>
2022-09-08 10:52:08 -0400

    registrar asistencias terminado, ahora hacer vista de reporte de consolidacion

--- vista/listar-celula-consolidacion.php
+++ vista/listar-celula-consolidacion.php
@@ -309,7 +309,7 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <form class="form" method="post" id="agregar_asistencias" action="?pagina=listar-celula-discipulado">
+          <form class="form" method="post" id="agregar_asistencias" action="?pagina=listar-celula-consolidacion">
             <div class="mb-3 row">
               <div id="grupo__asistentes" class="col-sm ">
                 <div class="relative">
