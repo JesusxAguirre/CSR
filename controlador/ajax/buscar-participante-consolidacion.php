37a93f0e1a37c78f15fc718efc4f46b4c15f3d4a
JesusxAguirre <99jesus.aguirre@gmail.com>
2022-09-15 07:02:02 -0400

    agregando buscar participante consolidacion en carpeta ajax

---
+++ controlador/ajax/buscar-participante-consolidacion.php
@@ -0,0 +1,25 @@
+<?php
+require_once('../../modelo/clase_celula_consolidacion.php');
+$objeto = new Consolidacion();
+
+$busqueda = $_GET['busqueda'];
+$matriz_participantes = $objeto->listar_participantes($busqueda);
+?>
+<?php if (!empty($matriz_participantes)) : ?>
+  <!-- Modal eliminar usuario -->
+  <?php foreach ($matriz_participantes as $participante) : ?>
+    <tr role='row'>
+      <td hidden class="id" role='cell'><?php echo $participante['id'] ?></td>
+      <td class="codigo" role='cell'><?php echo $participante['codigo_celula'] ?></td>
+      <td class="participantes_nombre" role='cell'><?php echo  $participante['participantes_nombre'] ?></td>
+      <td class="participantes_apellido" role='cell'><?php echo $participante['participantes_apellido'] ?></td>
+      <td class="participantes_codigo" role='cell'><?php echo  $participante['participantes_codigo'] ?></td>
+      <td class="participantes_telefono" role='cell'><?php echo  $participante['participantes_telefono'] ?></td>
+      <td hidden class="participantes_cedula" role="cell"><?php echo $participante['participantes_cedula'] ?></td>
+      <td role="cell">
+        <button type="submit" data-bs-toggle="modal" data-bs-target="#eliminar" class="btn btn-outline-danger delete-btn" name="eliminar_participantes" class="btn btn-outline-danger delete-btn"><i class="fs-5 bi bi-trash-fill"></i></button>
+      </td>
+    </tr>
+  <?php endforeach;       ?>
+  <!-- Modal eliminar usuario -->
+<?php endif ?>
