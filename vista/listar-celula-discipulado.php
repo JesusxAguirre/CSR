128c5103938e405c702bda093cd62d09c36d42c9
JesusxAguirre <99jesus.aguirre@gmail.com>
2022-09-27 10:45:47 -0400

    agregando permisos en btn de actualizar en celula de discipulado

--- vista/listar-celula-discipulado.php
+++ vista/listar-celula-discipulado.php
@@ -83,7 +83,9 @@
                         <td class="anfitrion" role='cell'><?php echo  $celula['codigo_anfitrion'] ?></td>
                         <td class="asistente" role='cell'><?php echo  $celula['codigo_asistente'] ?></td>
                         <td class="" role="cell">
+                        <?php   if ($_SESSION['permisos']['celula_discipulado']['actualizar'] > 0) :?>
                           <button type="button" data-bs-toggle="modal" data-bs-target="#editar" class="btn btn-outline-primary edit-btn"><i class="fs-5 bi bi-pencil-fill"></i></button>
+                          <?php endif; ?>
                           <button type="button" data-bs-toggle="modal" data-bs-target="#agregar_usuario" class="btn btn-outline-primary agregar-btn"> <i class=" fs-5 bi bi-person-plus-fill"></i> </button>
                           <button type="button" data-bs-toggle="modal" data-bs-target="#agregar_asistencia" class="btn btn-outline-primary asistencias-btn"> <i class=" fs-5 bi bi-calendar-date-fill"></i> </button>
                           <button type="button" class="btn btn-outline-danger modal-btn "><i class="fs-5 bi bi bi-person-dash-fill"></i></button>
