1b467137f288ed1e0ac4038ea79954e81ee89ad4
JesusxAguirre <99jesus.aguirre@gmail.com>
2022-10-11 05:32:45 -0400

    creando archivos para vista de perfil de usuarios

---
+++ vista/mi-perfil.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Mi perfil</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=0.6">
+
+
+  <!-- Bostrap 5 -->
+  <link rel="stylesheet" href="resources/css/bootstrap.min.css">
+  <link rel="stylesheet" href="resources/css/style.css">
+  <link rel="stylesheet" href="vendor/twbs/bootstrap-icons/font/bootstrap-icons.css">
+
+  <!-- Jquery-->
+  <script src="resources/js/jquery-3.6.0.min.js"></script>
+
+  <!-- Js boostrap -->
+  <script src="resources/js/bootstrap.min.js"></script>
+  <!-- CHOICE 2 -->
+  <link rel="stylesheet" href="resources/library/choice/public/assets/styles/choices.min.css">
+  <script src="resources/library/choice/public/assets/scripts/choices.min.js"></script>
+  <!-- Estilos de validacion-->
+  <link rel="stylesheet" href="resources/css/listar-consolidacion.css">
+  <!-- Sweet alert 2-->
+  <script src="resources/js/sweetalert2.js"></script>
+</head>
+
+<body>
+
+  <!-- Menu.php -->
+  <?php
+  require_once ("resources/View_Components/Menu.php")
+  ?>
+  <!-- Menu.php -->
+  <!-- sidebar.php -->
+  <?php
+  require_once "resources/View_Components/Sidebar.php";
+  ?>
+  <!-- sidebar.php -->
+  <main style="height: 100vh" class="pt-3">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <div class="page-title-box">
+            <h4 class="page-title">Mi perfil</h4>
+          </div>
+        </div>
+      </div>
+
+</body>
