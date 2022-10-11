b5cdc6509b660900f1967f86df742a901f865ee1
JesusxAguirre <99jesus.aguirre@gmail.com>
2022-10-11 06:25:31 -0400

    listo los estilos de mi perfil

--- vista/mi-perfil.php
+++ vista/mi-perfil.php
@@ -44,7 +44,101 @@
       <div class="mb-3 card">
         <div class="mb-8 position-relative min-vh-25 mb-7 card-header">
           <div class="bg-holder rounded-3 rounded-bottom-0" style="background-image: url(resources/img/paisaje.jpg);"></div>
-          <div class="avatar avatar-5xl avatar-profile"><img class="rounded-circle img-thumbnail shadow-sm" src="resources/img/casadark.jpg" alt=""></div>
+          <div class="avatar avatar-5xl avatar-profile">
+            <img class="d-inline-block align-text-top rounded-circle shadow-sm"  width="120" height="124" src="resources/img/mi-foto.jpg" alt="">
+          </div>
+      
+        </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-lg-8">
+              <h4 class="mb-1">Anthony Hopkins </span></h4>
+              <h5 class="fs-0 fw-normal"></h5>
+              <div class="border-dashed border-bottom my-4 d-lg-none"></div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="mt-2 col">
+          <div class="card">
+            <div class="card-body">
+              <form id="formulario" class="" method="POST" action="?pagina=registrar-casa">
+                <div class="mb-3 row">
+                  <div id="grupo__lider" class="col-sm ">
+                    <div class="relative">
+                      <label class="form-label fw-bold" for="">Selecciona el lider que esta abriendo la casa sobre la roca</label>
+                      <i class="input-icon fs-5"></i>
+                      <select name="lider[]" id="lider" class="form-control">
+                        <option value="">Seleccione una opcion</option>
+                        <?php foreach ($matriz_lider as $lider) : ?>
+                          <option value="<?php echo $lider['cedula']; ?>"> <?php echo $lider['codigo']; ?></option>
+                        <?php endforeach; ?>
+                      </select>
+                    </div>
+                    <p class="text-danger d-none">Este campo no puede estar vacio</p>
+                  </div>
+                </div>
+                <div class="mb-3 row">
+                  <div id="grupo__direccion" class="col-sm ">
+                    <div class="relative">
+                      <label class="form-label fw-bold" for="formGridEmail">Direccion</label>
+                      <i class="input-icon fs-5"></i>
+                      <input placeholder="Calle 19 con calle 40" type="text" name="direccion" class="form-control">
+                    </div>
+                    <p class="text-danger d-none">Este campo no puede estar vacio</p>
+                  </div>
+                  <div id="grupo__nombre" class="col-sm ">
+                    <div class="relative">
+                      <label class="form-label fw-bold" for="formGridEmail">Nombre de Anfitrion</label>
+                      <i class="input-icon fs-5"></i>
+                      <input placeholder="Juan Jimenez" type="text" id="" name="nombre" class="form-control">
+                    </div>
+                    <p class="text-danger d-none">Este campo lleva minimo 3 letras</p>
+                  </div>
+                  <div id="grupo__telefono" class="col-sm ">
+                    <div class="relative">
+                      <label class="form-label fw-bold" for="formGridEmail">Telefono de Anfitrion</label>
+                      <i class="input-icon fs-5"></i>
+                      <label class="form-label fw-bold" for="formGridPassword">Telefono de Anfitrion</label>
+                      <input placeholder="0414-XXXXXXXX" type="text" id="" name="telefono" class="form-control">
+                    </div>
+                    <p class="text-danger d-none">Escriba un numero de telefono valido</p>
+                  </div>
+                  <div class="mb-3 row mt-4">
+                    <div id="grupo__dia" class="col-sm ">
+                      <div class="relative">
+                        <label class="form-label fw-bold" for="formGridEmail">Dia de visita</label>
+                        <i class="input-icon fs-5"></i>
+                        <input placeholder="Jueves" id="dia" name="dia" class="form-control">
+                      </div>
+                      <p class="text-danger d-none">Escriba un dia de la semana con la primera letra mayuscula</p>
+                    </div>
+                    <div id="grupo__hora" class="col-sm ">
+                      <div class="relative">
+                        <label class="form-label fw-bold">Hora pautada</label>
+                        <i class="input-icon fs-5"></i>
+                        <input type="time" placeholder="8:30" id="197" name="hora" class="form-control">
+                      </div>
+                      <p class="text-danger d-none">Este campo no puede estar vacio</p>
+                    </div>
+                    <div id="grupo__integrantes" class="col-sm ">
+                      <div class="relative">
+                        <label class="form-label fw-bold" for="">Número de personas que integran el hogar</label>
+                        <i class="input-icon fs-5"></i>
+                        <input placeholder="1" name="integrantes" class="form-control">
+                      </div>
+                      <p class="text-danger d-none">Este campo no puede estar vacio</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="mb-3" id="">
+                </div>
+                <button name="registrar" type="submit" class="btn btn-primary">Enviar</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
