5497cdc20cc69aa286166bd19ccee23c5568f0e5
JesusxAguirre <99jesus.aguirre@gmail.com>
2022-10-13 21:57:39 -0400

    controlador de envio de correo listo

--- controlador/envio-correo.php
+++ controlador/envio-correo.php
@@ -5,13 +5,14 @@ require_once("modelo/clase_correo.php");
 session_start();
 
 if($_SESSION['verdadero'] > 0){
-    if ($_SESSION['rol'] != 1) {
+    if (!$_SESSION['permisos']['envio_correo']['listar']) {
         echo "<script>
 		alert('No tienes los permisos para este modulo');
 		window.location= 'index.php?pagina=mi-perfil'
 		</script>";
 
     }
+      
 if (is_file('vista/'.$pagina.'.php')) {
   $objeto = new Correo();
 
