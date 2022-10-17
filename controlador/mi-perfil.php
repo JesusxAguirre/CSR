68304078edec350727e27ac3148da010faa9d250
JesusxAguirre <99jesus.aguirre@gmail.com>
2022-10-17 16:57:22 -0400

    acomodando controlador de mi perfil

--- controlador/mi-perfil.php
+++ controlador/mi-perfil.php
@@ -1,15 +1,15 @@
 <?php
-r//destruye la sesion si se tenia una abierta
+    require_once("modelo/clase_usuario.php");
+//destruye la sesion si se tenia una abierta
 session_start();
 
 if ($_SESSION['verdadero'] > 0) {
     if (is_file('vista/' . $pagina . '.php')) {
-        require_once("modelo/clase_usuario.php");
+    
         $objeto = new Usuarios();
         $matriz_usuario = $objeto->mi_perfil();
 
-        $accion = 'El usuario ha entrado a "Mi Perfil"';
-        $objeto->registrar_bitacora($accion);
+
 
         foreach ($matriz_usuario as $usuario) {
             $nombre = $usuario['nombre'];
@@ -63,8 +63,8 @@ if ($_SESSION['verdadero'] > 0) {
     }
 } else {
     echo "<script>
-           window.location= 'error.php'
-;
+           window.location= 'error.php'";
+
     echo "<script>
     alert('Sesion Cerrada');
     window.location= 'index.php'
