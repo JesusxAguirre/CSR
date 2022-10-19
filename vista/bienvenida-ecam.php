ef4b61206ec4f33318f7484ba7e4f42505e5ec0f
JesusCAN3LON <64654517+JesusCAN3LON@users.noreply.github.com>
2022-10-19 01:46:47 -0400

    otro2

--- vista/bienvenida-ecam.php
+++ vista/bienvenida-ecam.php
@@ -11,7 +11,7 @@
                 <div class="bg-dark py-2">
 
                     <ul class="nav justify-content-center">
-                        <?php if($_SESSION['status_profesor'] == 0){ ?>
+                        <!-- <?php if($_SESSION['status_profesor'] == 0){ ?>
                         <li class="nav-item">
                             <a class="nav-link fs-6 btn btn-success mx-1 text-white font-monospace" href="?pagina=aula-virtual-Est">Inicio</a>
                         </li>
@@ -20,25 +20,29 @@
                         <li class="nav-item">
                             <a class="nav-link fs-6 btn btn-success mx-1 text-white font-monospace" href="?pagina=aula-virtual-Prof">Inicio</a>
                         </li>
-                        <?php } ?>
+                        <?php } ?> -->
+
                         <?php if ($_SESSION['status_profesor'] == 0){ ?>
                         <li class="nav-item">
-                            <a class="nav-link fs-6 btn btn-success mx-1 text-white font-monospace" href="?pagina=mis-materiasEstudiante">Mis materias</a>
+                            <a class="nav-link fs-6 btn btn-success mx-1 text-white font-monospace" href="?pagina=mis-materiasEstudiante">Mis Materias</a>
                         </li>
                         <?php } ?>
+
                         <?php if ($_SESSION['status_profesor'] == 1){ ?>
                         <li class="nav-item">
-                            <a class="nav-link fs-6 btn btn-success mx-1 text-white font-monospace" href="?pagina=mis-materiasProfesor">Mis materias</a>
+                            <a class="nav-link fs-6 btn btn-success mx-1 text-white font-monospace" href="?pagina=mis-materiasProfesor">Mis Materias</a>
                         </li>
                         <?php } ?>
+
                         <?php if ($_SESSION['status_profesor'] == 0){ ?>
                         <li class="nav-item">
-                            <a class="nav-link fs-6 btn btn-success mx-1 text-white font-monospace" href="?pagina=mis-notas">Mis notas</a>
+                            <a class="nav-link fs-6 btn btn-success mx-1 text-white font-monospace" href="?pagina=mis-notas">Mis Notas Materias</a>
                         </li>
                         <?php } ?>
+                        
                         <?php if ($_SESSION['status_profesor'] == 1){ ?>
                         <li class="nav-item">
-                            <a class="nav-link fs-6 btn btn-success mx-1 text-white font-monospace" href="?pagina=mis-estudiantes">Mis estudiantes</a>
+                            <a class="nav-link fs-6 btn btn-success mx-1 text-white font-monospace" href="?pagina=mis-estudiantes">Mis Estudiantes</a>
                         </li>
                         <?php } ?>
                     </ul>
