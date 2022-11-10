33c4819d0ea55fce06803b5cef96cbf8d05d6304
JesusxAguirre <99jesus.aguirre@gmail.com>
2022-11-10 18:18:08 -0400

    arreglando error de celula de discipulado

--- controlador/ajax/buscar-participante-asistencias-consolidacion.php
+++ controlador/ajax/buscar-participante-asistencias-consolidacion.php
@@ -1,15 +1,14 @@
 <?php
 require_once('../../modelo/clase_celula_consolidacion.php');
 $objeto = new Consolidacion();
-
+session_start();
 $busqueda = $_GET['busqueda'];
 $matriz_participantes = $objeto->listar_participantes($busqueda);
 
 ?>
-<?php if (!empty($matriz_participantes)) : ?>
+
   <select multiple name="asistentes[]" id="asistentes" class="form-control">             
   <?php foreach ($matriz_participantes as $participante) : ?>
   <option value="<?php echo $participante['participantes_cedula']; ?>"> <?php echo $participante['participantes_codigo']; ?></option>
   <?php endforeach;       ?>
   </select>
-<?php endif ?>
