5dcd90580ba86096763d8b8ec595e43dc2be33c8
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-14 12:47:15 -0400

    modificando nuevamente el js

--- resources/js/chat-aguirre.js
+++ resources/js/chat-aguirre.js
@@ -1,6 +1,8 @@
 //creando objeto websocket
 var msgBox = document.getElementById('areaChat')
 var objeto_websocket = new WebSocket("ws://localhost:8080");
+var formulario = document.getElementById('chatForm');
+var mensaje = document.getElementById('mensaje');
 
 objeto_websocket.onopen = function (e) {//cuando la conexion se abre 
   console.log("conexion establecida!")
@@ -8,26 +10,17 @@ objeto_websocket.onopen = function (e) {//cuando la conexion se abre
 }
 
 objeto_websocket.onmessage = function (e) {
-  var response = JSON.parse(e.data) //PHP envia datos en formato json
+  console.log(e.data);
+  var data = JSON.parse(e.data);
+  var div = document.createElement('div');
+  div.innerHTML = data.mensaje;
+  div.className = 'alert alert-primary';
+  div.role = 'alert';
 
-  var res_type = response.type //tipo de mensaje
-  var user_message = response.message //texto de mensaje
-  var user_name = response.name // nombre del usuario que envia el mensaje
-  var user_color = response.color
-
-  switch (res_type) {
-    case 'usermsg':
-      msgBox.append('<div><span class="user_name" style="color:' + user_color + '">' + user_name + '</span> : <span class="user_message">' + user_message + '</span></div>');
-      break;
-    case 'system':
-      msgBox.append('<div style="color:#bbbbbb">' + user_message + '</div>');
-      break;
-  }
-  msgBox[0].scrollTop = msgBox[0].scrollHeight; //scroll message
+  document.getElementById('areaChat').append(div);
 }
 
-var formulario = document.getElementById('chatForm');
-var mensaje = document.getElementById('mensaje');
+
 
 formulario.addEventListener('submit', function(e) {
     e.preventDefault();
