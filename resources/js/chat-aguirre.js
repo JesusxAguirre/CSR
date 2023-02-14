48a84f50731ffec3a6d9415c907b72482f03e5e7
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-14 13:25:06 -0400

    ya esta funcionando toca cambiar funcionalidades y hacer pruebas para entender mejor como funciona

--- resources/js/chat-aguirre.js
+++ resources/js/chat-aguirre.js
@@ -1,12 +1,14 @@
+$(document).ready(function (e) {
 //creando objeto websocket
-var msgBox = document.getElementById('areaChat')
+var msgBox = document.getElementById('areaChat');
 var objeto_websocket = new WebSocket("ws://localhost:8080");
 var formulario = document.getElementById('chatForm');
 var mensaje = document.getElementById('mensaje');
 
 objeto_websocket.onopen = function (e) {//cuando la conexion se abre 
-  console.log("conexion establecida!")
-  msgBox.append("<div  style='color:#bbbbbb'>Welcome to my Demo WebSocket Chat box!</div>") //notify user
+  console.log("conexion establecida!");
+  msgBox.append('<div class="system_msg" style="color:#bbbbbb">Welcome to my "Demo WebSocket Chat box"!</div>'); //notify user 
+
 }
 
 objeto_websocket.onmessage = function (e) {
@@ -17,7 +19,7 @@ objeto_websocket.onmessage = function (e) {
   div.className = 'alert alert-primary';
   div.role = 'alert';
 
-  document.getElementById('areaChat').append(div);
+  msgBox.append(div);
 }
 
 
@@ -31,6 +33,34 @@ formulario.addEventListener('submit', function(e) {
             mensaje: mensaje,
         }
 
-        conn.send(JSON.stringify(data));
+        objeto_websocket.send(JSON.stringify(data));
     }
 })
+
+
+  //////////Validaciones de mensaje vacio
+  const expresiones = {
+    expresioness: /^[a-zA-ZÀ-ÿ0-9\s]{1,10000}$/, // Letras y espacios, pueden llevar acentos.
+}
+
+var campo = {
+    existe: false,
+}
+
+function validar(evento) {
+    if (expresiones.expresioness.test(evento.value)) {
+        document.getElementById('enviar').removeAttribute('disabled');
+        campo[0]= true;
+    }else{
+        document.getElementById('enviar').setAttribute('disabled', "");
+        campo[0] = false;
+    }
+}
+
+mensaje.addEventListener('keyup', function(e) {
+    validar(mensaje);
+});
+//////////Fin de la validacion
+
+
+});
--- src/Chat.php
+++ src/Chat.php
@@ -13,10 +13,10 @@ class Chat implements MessageComponentInterface {
     }
   
     public function onOpen(ConnectionInterface $conn) {
-    $this->clients->attach($conn);
-    $fecha_actual = date("d-m-Y h:i:s");
+      $this->clients->attach($conn);
+      $fecha_actual = date("d-m-Y h:i:s");
 
-    echo "Nueva conexion $fecha_actual ({$conn->resourceId})\n";
+      echo "Nueva conexion $fecha_actual ({$conn->resourceId})\n";
     }
     
 
@@ -24,7 +24,7 @@ class Chat implements MessageComponentInterface {
       $numRecv = count($this->clients) - 1;
       echo sprintf('El usuario %d esta enviando el mensaje: "%s" to %d other connection%s' . "\n"
       , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
+      
       foreach($this->clients as $client){
         if ($from !== $client){
           $client->send($msg);
