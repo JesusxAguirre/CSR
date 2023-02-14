fba65685c5c9c5fc8938c4bc9b4e91eb8dcbde27
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-14 11:56:00 -0400

    intentando editar datos con js

---
+++ clase_chat/clase_chat.php
@@ -0,0 +1,19 @@
+<?php 
+namespace MyApp;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Chat implements MessageComponentInterface {
+    public function onOpen(ConnectionInterface $conn) {
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+    }
+}
+?>
