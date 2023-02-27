55a971d04658207c26f50bdfd25fe7cd6f556559
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-02-27 10:05:29 -0400

    construyendo primer caso de prueba login

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
+use Csr\Modelo\Usuarios;
+
 
 final class LoginTest extends TestCase
 {
@@ -7,11 +9,15 @@ final class LoginTest extends TestCase
     /** @test **/
     public function test_login_admin ()
     {
-      $suma = 2 + 3 ;
-      $expected = 4;
-        $this->assertEquals(
-            $expected ,
-            $suma
-        );
+     //Init
+      $objeto_usuarios = New Usuarios();
+      $_SESSION['usuario'] = "casasobrelaroca@gmail.com";
+      $_SESSION['clave'] = "987654321";
+      $expected = 1;
+     //Act  
+      $response = $objeto_usuarios->validar();
+
+     //Asert
+     $this->assertEquals($expected,$objeto_usuarios);
     }
 }
