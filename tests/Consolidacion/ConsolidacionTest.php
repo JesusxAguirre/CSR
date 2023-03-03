48b1f5439f65dc3c969867dcd03341af6994a61c
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-03-02 23:42:42 -0400

    testeando registro de consolidacion

--- tests/Consolidacion/ConsolidacionTest.php
+++ tests/Consolidacion/ConsolidacionTest.php
@@ -13,6 +13,7 @@ final class UsuariosTest extends TestCase
   public function setUp(): void
   {
     $this->objeto_consolidacion   = new Consolidacion();
+    $_SESSION['cedula'] = 27666555;
   }
   /** @test **/
   public function test_listar_usuarios_N2(): array
@@ -23,8 +24,10 @@ final class UsuariosTest extends TestCase
     $array_usuarios_n2_3 =$this->objeto_consolidacion->listar_usuarios_N2();
 
     //Assert
+ 
+    $this->assertArrayHasKey($key_expected,$array_usuarios_n2_3[0]);
 
-    $this->assertArrayHasKey($key_expected,$array_usuarios_n2_3);
+  
 
     return $array_usuarios_n2_3;
   }
@@ -34,11 +37,12 @@ final class UsuariosTest extends TestCase
     //Init
     $key_expected = "cedula";
     //Act
-    $array_usuarios_a_consolidar =$this->objeto_consolidacion->listar_usuarios_N2();
+    $array_usuarios_a_consolidar =$this->objeto_consolidacion->listar_no_participantes();
 
     //Assert
 
-    $this->assertArrayHasKey($key_expected,$array_usuarios_a_consolidar);
+    
+    $this->assertArrayHasKey($key_expected,$array_usuarios_a_consolidar[0]);
 
     return $array_usuarios_a_consolidar;
   }
@@ -48,8 +52,34 @@ final class UsuariosTest extends TestCase
    * @depends test_listar_no_participantes
    * @depends test_listar_usuarios_N2
    * **/
-  public function test_registrar_consolidacion(){
-    
+  public function test_registrar_consolidacion($array_usuarios_a_consolidar, $array_usuarios_n2_3){
+    //Init
+    $dia_reunion = "Jueves";
+    $hora = date("h:i:s");
+    $direccion_celula = "Villa crepuscular";
+
+    foreach($array_usuarios_n2_3 as $usuarios){
+      $cedulas_n2 = $usuarios['cedula'];
+    }
+    //Act
+   
+    $this->objeto_consolidacion->setConsolidacion($cedulas_n2[0],$cedulas_n2[1],
+    $cedulas_n2[2],$dia_reunion,$hora,$direccion_celula,$array_usuarios_a_consolidar);
+
+    $this->objeto_consolidacion->registrar_consolidacion();
+
+    $array_celulas_consolidacion = $this->objeto_consolidacion->listar_celula_consolidacion();
+    //Assert
+    print_r($array_celulas_consolidacion);
+    foreach($array_celulas_consolidacion as $celulas_consolidacion){
+      if($cedulas_n2[0] == $celulas_consolidacion['cedula_lider']){
+        $celula_consolidacion_nueva = $celulas_consolidacion;
+      }
+    }
+
+    $this->assertContains($cedulas_n2,$celula_consolidacion_nueva);
+
+    return $celula_consolidacion_nueva;
   }
 
 }
