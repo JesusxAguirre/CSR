3625ba6462575d2fa19b52eedc124694f39fc550
JesusxAguirre <99jesus.aguirre@gmail.com>
2023-07-13 20:17:43 -0400

    esta parcialmente correcto , hay que hacer mas pruebas

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 require_once("vendor/autoload.php");
+
+use Csr\Modelo\Usuarios;
+
+	$objeto_usuarios = new Usuarios();
+
+	$objeto_usuarios->check_blacklist();
+
+	$objeto_usuarios->check_requests_danger();
+
 	if (empty($_GET['pagina'])) {
 		
 		$pagina = "inicio";
